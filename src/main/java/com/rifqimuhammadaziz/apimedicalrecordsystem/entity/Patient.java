a29a2f7549b30b89cd1c255a4bd7ef5bf8ec82a5
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-10-31 14:42:00 +0700

    change json property for patient

--- src/main/java/com/rifqimuhammadaziz/apimedicalrecordsystem/entity/Patient.java
+++ src/main/java/com/rifqimuhammadaziz/apimedicalrecordsystem/entity/Patient.java
@@ -17,20 +17,31 @@ import java.util.Date;
 public class Patient {
 
     @Id
-//    @NotNull(message = "ID Number is required")
-//    @NotBlank(message = "ID Number is Blank")
+    @Column(name = "id_number")
     private String idNumber;
 
-//    @NotBlank(message = "Name is required")
+    @Column(name = "full_name")
     private String fullName;
 
+    @Column(name = "birthdate")
     private Date birthdate;
 
+    @Column(name = "age")
     private int age;
+
+    @Column(name = "gender")
     private String gender;
+
+    @Column(name = "blood_group")
     private String bloodGroup;
+
+    @Column(name = "religion")
     private String religion;
+
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "citizenship")
     private String citizenship;
 
 }
--- src/main/java/com/rifqimuhammadaziz/apimedicalrecordsystem/model/request/PatientDTO.java
+++ src/main/java/com/rifqimuhammadaziz/apimedicalrecordsystem/model/request/PatientDTO.java
@@ -1,18 +1,37 @@
 package com.rifqimuhammadaziz.apimedicalrecordsystem.model.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
 public class PatientDTO {
+
+    @JsonProperty("id_number")
     private String idNumber;
+
+    @JsonProperty("full_name")
     private String fullName;
+
+    @JsonProperty("birthdate")
     private Date birthdate;
+
+    @JsonProperty("age")
     private int age;
+
+    @JsonProperty("gender")
     private String gender;
+
+    @JsonProperty("blood_group")
     private String bloodGroup;
+
+    @JsonProperty("religion")
     private String religion;
+
+    @JsonProperty("address")
     private String address;
+
+    @JsonProperty("citizenship")
     private String citizenship;
 }
