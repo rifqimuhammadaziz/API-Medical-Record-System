b6631bc0a388811364dc08716dda6cf8adf5d9b5
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-11-23 08:40:33 +0700

    update logs

---
+++ src/main/java/com/rifqimuhammadaziz/apimedicalrecordsystem/configuration/SecurityConfiguration.java
@@ -0,0 +1,4 @@
+package com.rifqimuhammadaziz.apimedicalrecordsystem.configuration;
+
+public class SecurityConfiguration {
+}
--- src/main/java/com/rifqimuhammadaziz/apimedicalrecordsystem/controller/PatientController.java
+++ src/main/java/com/rifqimuhammadaziz/apimedicalrecordsystem/controller/PatientController.java
@@ -6,61 +6,41 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import lombok.AllArgsConstructor;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/patients")
+@Tag(name = "Patient Controller", description = "Controller for patient resources")
 public class PatientController {
 
     private final PatientService patientService;
 
     @Operation(summary = "Save Patient", description = "API for save of patient")
     @ApiResponses(value = {
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "Success insert patient to database",
-                    content = @Content(mediaType = "application/json")
-            ),
-            @ApiResponse(
-                    responseCode = "400",
-                    description = "Failed insert patient to database",
-                    content = @Content(mediaType = "application/json")
-            ),
-            @ApiResponse(
-                    responseCode = "500",
-                    description = "Internal server error",
-                    content = @Content(mediaType = "application/json")
-            )
+            @ApiResponse(responseCode = "2xx", description = "Success insert patient to database", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "4xx", description = "Failed insert patient to database", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "5xx", description = "Internal server error", content = @Content(mediaType = "application/json"))
     })
     @PostMapping()
     public Patient createPatient(@RequestBody @Valid Patient patient) {
+        log.info("[{}][REQUEST RECEIVED][{}]", patient.getIdNumber(), patient.getFullName());
         return patientService.createPatient(patient);
     }
 
     @Operation(summary = "Get All Patients", description = "API for fetch all patient from database")
     @ApiResponses(value = {
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "Success get all patients from database",
-                    content = @Content(mediaType = "application/json")
-            ),
-            @ApiResponse(
-                    responseCode = "400",
-                    description = "Failed get all patients from database",
-                    content = @Content(mediaType = "application/json")
-            ),
-            @ApiResponse(
-                    responseCode = "500",
-                    description = "Internal server error",
-                    content = @Content(mediaType = "application/json")
-            )
+            @ApiResponse(responseCode = "200", description = "Success get all patients from database", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "Failed get all patients from database", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = "application/json"))
     })
     @GetMapping()
     public List<Patient> findAllPatients() {
@@ -69,21 +49,9 @@ public class PatientController {
 
     @Operation(summary = "Get Single Patient By ID", description = "API for get single patient by ID from database")
     @ApiResponses(value = {
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "Success get patient from database",
-                    content = @Content(mediaType = "application/json")
-            ),
-            @ApiResponse(
-                    responseCode = "400",
-                    description = "Failed get patient from database",
-                    content = @Content(mediaType = "application/json")
-            ),
-            @ApiResponse(
-                    responseCode = "500",
-                    description = "Internal server error",
-                    content = @Content(mediaType = "application/json")
-            )
+            @ApiResponse(responseCode = "200", description = "Success get patient from database", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "Failed get patient from database", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = "application/json"))
     })
     @GetMapping("/{id}")
     public ResponseEntity<Patient> findSinglePatientByID(@PathVariable("id") String id) {
@@ -92,21 +60,9 @@ public class PatientController {
 
     @Operation(summary = "Update Patient", description = "API for update patient")
     @ApiResponses(value = {
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "Success update patient to database",
-                    content = @Content(mediaType = "application/json")
-            ),
-            @ApiResponse(
-                    responseCode = "400",
-                    description = "Failed update patient to database",
-                    content = @Content(mediaType = "application/json")
-            ),
-            @ApiResponse(
-                    responseCode = "500",
-                    description = "Internal server error",
-                    content = @Content(mediaType = "application/json")
-            )
+            @ApiResponse(responseCode = "200", description = "Success update patient to database", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "Failed update patient to database", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = "application/json"))
     })
     @PutMapping()
     public Patient updatePatient(@RequestBody Patient patient) {
@@ -115,21 +71,9 @@ public class PatientController {
 
     @Operation(summary = "Delete Patient By ID", description = "API for delete single patient by ID")
     @ApiResponses(value = {
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "Success insert patient to database",
-                    content = @Content(mediaType = "application/json")
-            ),
-            @ApiResponse(
-                    responseCode = "400",
-                    description = "Failed insert patient to database",
-                    content = @Content(mediaType = "application/json")
-            ),
-            @ApiResponse(
-                    responseCode = "500",
-                    description = "Internal server error",
-                    content = @Content(mediaType = "application/json")
-            )
+            @ApiResponse(responseCode = "200", description = "Success insert patient to database", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "400", description = "Failed insert patient to database", content = @Content(mediaType = "application/json")),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content(mediaType = "application/json"))
     })
     @DeleteMapping("/{id}")
     public void deleteSinglePatientByID(@PathVariable("id") String id) {
