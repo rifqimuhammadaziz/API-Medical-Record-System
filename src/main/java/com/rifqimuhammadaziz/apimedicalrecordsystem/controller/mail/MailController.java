66bd7a61e881cc6e0fcf0541564ef5ec1354e952
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-12-02 08:35:40 +0700

    create email sender

---
+++ src/main/java/com/rifqimuhammadaziz/apimedicalrecordsystem/controller/mail/MailController.java
@@ -0,0 +1,24 @@
+package com.rifqimuhammadaziz.apimedicalrecordsystem.controller.mail;
+
+import com.rifqimuhammadaziz.apimedicalrecordsystem.service.contract.MailService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/mail")
+@RequiredArgsConstructor
+public class MailController {
+
+    private final MailService mailService;
+
+    @GetMapping("/send")
+    public String sendEmailTest() {
+        mailService.sendMailTest();
+        log.info("[][EMAIL SUCCESSFULLY SENT]");
+        return "Success";
+    }
+}
---
+++ src/main/java/com/rifqimuhammadaziz/apimedicalrecordsystem/service/contract/MailService.java
@@ -0,0 +1,5 @@
+package com.rifqimuhammadaziz.apimedicalrecordsystem.service.contract;
+
+public interface MailService {
+    void sendMailTest();
+}
